c9cc3b7501f7aea501b1d33bee141f4c333e8f34
Prashant GIRENNAVAR <98313322+giteepag@users.noreply.github.com>
2022-04-21 14:44:00 +0530

    Update cfn-validate-template.sh

--- cfn-validate-template.sh
+++ cfn-validate-template.sh
@@ -1,1 +1,14 @@
+#/bin/bash
+
+file='/var/lib/jenkins/workspace/jenkins-test/deploy/LINUX-EC2.yaml'
+
+#Check if templates have syntax error
+aws cloudformation validate-template --template-body "file://$file"
+
+if($? --ne 0)
+then
+  echo 'Template sytax is validated successfully'
+ elif
+  echo 'Template has sytax errors!'
+fi
 
