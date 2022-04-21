5575aa4f8ad8647516a6b77437fb26f2bd95cdf9
Prashant GIRENNAVAR <98313322+giteepag@users.noreply.github.com>
2022-04-21 14:48:27 +0530

    Update cfn-validate-template.sh

--- cfn-validate-template.sh
+++ cfn-validate-template.sh
@@ -1,14 +1,15 @@
 #/bin/bash
 
+api_region="us-east-01"
 file='/var/lib/jenkins/workspace/jenkins-test/deploy/LINUX-EC2.yaml'
 
 #Check if templates have syntax error
-aws cloudformation validate-template --template-body "file://$file"
+aws cloudformation --region $api_region validate-template --template-body "file://$file"
 
 if($? -ne 0)
 then
   echo 'Template sytax is validated successfully'
- elif
+ else
   echo 'Template has sytax errors!'
 fi
 
