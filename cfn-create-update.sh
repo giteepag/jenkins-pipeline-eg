0d10230fb6949baf66f034dd54204b1c11da358e
Prashant GIRENNAVAR <98313322+giteepag@users.noreply.github.com>
2022-04-21 13:51:51 +0530

    Update cfn-create-update.sh

--- cfn-create-update.sh
+++ cfn-create-update.sh
@@ -12,7 +12,7 @@ else
     type_formation='update-stack'
 fi
 
-file='\deploy\LINUX-EC2.yaml'
+file='/deploy/LINUX-EC2.yaml'
 
 echo "======================================== Creating/Updating the stack "
 eval "aws --region $api_region cloudformation $type_formation --stack-name $api_stack_name --template-body 'file://$file'"
